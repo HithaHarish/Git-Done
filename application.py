2a5113ad6147485a28716edacd96c52c8139c924
HithaHarish <hitha22harish@gmail.com>
2025-10-30 23:06:46 +0530

    Removed Emoticons, Updated Debug = False

--- application.py
+++ application.py
@@ -746,4 +746,4 @@ def download_goal_ics(goal_id):
 if __name__ == '__main__':
     with application.app_context():
         db.create_all()
-    application.run(debug=True)
+    application.run(debug=False)
